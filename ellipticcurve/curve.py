53e0465e49c87f05a91152e8f0a84e466941d5db
Jonas Metzger <40152568+Jonas-Metzger@users.noreply.github.com>
2022-05-30 16:37:56 -0700

    Update curve.py

--- ellipticcurve/curve.py
+++ ellipticcurve/curve.py
@@ -62,9 +62,22 @@ prime256v1 = CurveFp(
 
 p256 = prime256v1
 
+p384 = CurveFp(
+    name="secp384r1",
+    nistName="P-384",
+    A=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000fffffffc, # P-3?
+    B=0xb3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef,
+    P=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000ffffffff,
+    N=0xffffffffffffffffffffffffffffffffffffffffffffffffc7634d81f4372ddf581a0db248b0a77aecec196accc52973,
+    Gx=0xaa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7,
+    Gy=0x3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f,
+    oid=[1, 3, 132, 0, 34],
+)
+
 supportedCurves = [
     secp256k1,
     prime256v1,
+    p384,
 ]
 
 _curvesByOid = {tuple(curve.oid): curve for curve in supportedCurves}
